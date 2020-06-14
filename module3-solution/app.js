1f1739ac2384d62938d55b8d3fb55ecea2b17374
Ritwika-Bhattacharjee <ritspure@gmail.com>
2020-06-14 13:43:29 +0530

    Adding Assignment 3 solutions

---
+++ module3-solution/app.js
@@ -0,0 +1,116 @@
+(function(){
+	'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive("foundItems", foundItemsDirective);
+
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'menuList.html',
+    //link: MenuItemsDirectiveLink,
+	scope: {
+      found: '<',
+      error: '<',
+      onRemove: '&'
+    },
+    controller: MenuItemsDirectiveController,
+    controllerAs: 'narrow',
+    bindToController: true,
+  };
+
+  return ddo;
+}
+
+
+function MenuItemsDirectiveController(){
+	var narrow = this;
+
+
+}
+
+function MenuItemsDirectiveLink(scope, element, attrs, controller){
+	/*scope.$watch('narrow.findSearchItem()', function(newValue, oldValue){
+		if(newValue==-1){
+			displayErrorMsg();
+		}else{
+			hideErrorMsg();
+		}
+	});
+
+	function displayErrorMsg(){
+		var errorElement = element.find("div");
+		element.css("display", "block");
+	}
+
+	function hideErrorMsg(){
+		var errorElement = element.find("div");
+		element.css("display", "none");
+	}*/
+}
+
+NarrowItDownController.$inject['MenuSearchService'];
+function NarrowItDownController(MenuSearchService){
+	var narrow = this; 
+
+	narrow.searchTerm = "";
+	narrow.found=[];
+	narrow.error=false;
+	narrow.findSearchItem = function(){
+
+		MenuSearchService.getMatchedMenuItems(narrow.searchTerm).then(function(response){
+			if(narrow.searchTerm.length==0 || response.length==0){
+				narrow.error=true;
+				narrow.found=[];
+			}
+			else{
+				narrow.error=false;
+				narrow.found=response;			
+			}
+			console.log(narrow.found);
+
+		});
+		
+	}
+
+	narrow.removeItems = function(itemIndex){
+		narrow.found.splice(itemIndex, 1);
+		if(narrow.found.length==0){
+			narrow.error = true;
+		}
+	}
+
+}
+
+
+MenuSearchService.$inject['$http'];
+function MenuSearchService($http){
+	var service = this;
+
+	service.getMatchedMenuItems= function(searchTerm){
+
+	return $http({
+				method: "GET",
+				url: ('https://davids-restaurant.herokuapp.com/menu_items.json')
+			})
+			.then(function (result) {
+
+    			// process result and only keep items that match
+
+    			var foundItems = [];
+    			for(var i=0; i<result.data.menu_items.length; i++){
+    				var itemdesc = result.data.menu_items[i].description;
+    				if(itemdesc.toLowerCase().indexOf(searchTerm)!=-1){
+    					foundItems.push(result.data.menu_items[i]);
+    				}
+    			}
+
+		    	//return processed items
+		    	return foundItems;
+	});
+
+	}
+}
+})();
